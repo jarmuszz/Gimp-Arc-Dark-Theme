ef39f4cf56cf417deaf8db8eb92a2f66a0694870
vxooo <mariuszjakoniuk2004@gmail.com>
2017-10-27 15:00:13 +0200

    frist commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# Gimp Arc Dark Theme
+Made for GIMP 2.8
+
+# Installation
+## For linux
+
+ **Using script**
+  1. Open terminal
+  2. Run script
+    * ~/Downloads/Gimp-Theme/Gimp-Arc-Dark-Theme/install.sh
+  3. Folow instructions
+  
+**Manualy**
+
+  #### Only for current user
+  1.Open terminal
+  2. Copy Downloaded folder
+  * cp -r ~/Downloads/Gimp-Theme/Gimp-Arc-Dark-Theme ~/.gimp-2.8/themes/
+  3. Change theme in gimp
+  * Edit -> Preferences -> Themes
+  
+  #### For every user
+  1.Open terminal
+  2. Copy Downloaded folder
+  * U need to be administrator for this
+  * sudo cp -r ~/Downloads/Gimp-Theme/Gimp-Arc-Dark-Theme /usr/share/gimp/*/themes/
+  3. Change theme in gimp
+  * Edit -> Preferences -> Themes
+  
+## For Mac OS X
+  1. Copy downloaded folder with theme (Gimp-Arc-Dark-Theme) to 
+  * ~/Library/Application Support/GIMP/2.8/themes
+  * or
+  * ~/.gimp-2.8/themes
+  2. Change theme in gimp
+  * Edit -> Preferences -> Themes
+    
+## For Windows
+  1. Copy downloaded theme (Gimp-Arc-Dark-Theme) folder to
+  * C:\Users<your user name>.gimp-2.8\themes
+  2. Change theme in gimp
+  * Edit -> Preferences -> Themes
+  
+## Notes
+* Theme is based on a "Gimp-CC-Theme-Dark" made by draekko
+  https://github.com/draekko/gimp-cc-themes/tree/master/Gimp-CC-Theme-Darker
+  
+  
+## Screenshots
+
+
---
+++ install.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+echo Arc-Dark theme for Gimp installer
+echo 1. Install only for you
+echo 2. Install for every user
+read i
+if [ "$i" = "1" ]
+then
+    echo
+    echo Files will be copied to ~/.gimp-2.8/themes/
+    cp -r ./Gimp-Arc-Dark-Theme ~/.gimp-2.8/themes/
+    rm ~/.gimp-2.8/themes/Gimp-Arc-Dark-Theme/install.sh
+        if [ -d ~/.gimp-2.8/themes/Gimp-Arc-Dark-Theme ]
+    then
+        echo
+        echo Successful copied files
+        echo To change theme u need go to:
+        echo Edit -\> Preferences -\> Theme
+    else
+        echo Failed to copy files
+    fi
+
+fi
+
+if [ "$i" = "2" ]
+then
+    echo 
+    echo Files will be copied to /usr/share/gimp/*/themes/
+    echo U need to be an administrator to copy files there
+    sudo cp -r ./Gimp-Arc-Dark-Theme /usr/share/gimp/*/themes/
+    sudo rm /usr/share/gimp/*/themes/Gimp-Arc-Dark-Theme/install.sh
+
+    if [ -d /usr/share/gimp/*/themes/Gimp-Arc-Dark-Theme ]
+    then
+        echo
+        echo Successful copied files
+        echo To change theme u need go to:
+        echo Edit -\> Preferences -\> Theme
+    else
+        echo Failed to copy files
+    fi
+fi
+if [ "$i" -lt "1" ]
+then
+    echo
+    echo Error:
+    echo Chose 1 or 2
+fi
+
+if [ "$i" -gt "2" ]
+then
+    echo
+    echo Error:
+    echo Chose 1 or 2
+fi
